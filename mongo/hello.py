44303192adb0ec673fb285521034556aef0eb580
strawbrea <breannalowery8@gmail.com>
2024-02-26 18:27:39 -0500

    update hello.py

--- mongo/hello.py
+++ mongo/hello.py
@@ -2,23 +2,27 @@ from flask import Flask, jsonify, send_from_directory
 import json
 from mongita import MongitaClientDisk
 
-app = Flask(__name__)
-
-
 # create a mongita client connection
 client = MongitaClientDisk()
 
-# create a movie database
+# open then movie database
 movie_db = client.movie_db
 
-# create a scifi collection
-scifi_collection = movie_db.scifi_collection
+app = Flask(__name__)
 
 @app.route("/data/movies/scifi")
 def get_data_movies_scifi():
-   # with open("","r") as f:
-    #    data =json.load(f)
-        data = list(scifi_collection.find({}))
-        for item in data:
-                del item["_id"]
-        return jsonify(data)
+    # with open("classic_sci_fi_movies.json","r") as f:
+    #     data = json.load(f)
+    # open the scifi collection
+    scifi_collection = movie_db.scifi_collection
+    data = list(scifi_collection.find({})) 
+    print(data)  
+    for item in data:
+        del item["_id"] 
+    print(data)
+    return jsonify(data)
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('.', path)
